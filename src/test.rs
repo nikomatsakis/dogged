5cd1be3ca1fd3fd7fc20d7295c28fa05b721b463
Niko Matsakis <niko@alum.mit.edu>
2016-11-21 18:00:43 -0500

    add some tests that show we are persistent
    and also that we don't clone more memory than we ought to

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,5 @@
 use super::PersistentVec;
+use super::BRANCH_FACTOR;
 
 #[test]
 fn push_matches_len() {
@@ -11,5 +12,64 @@ fn push_matches_len() {
 
     for i in 0..N {
         assert_eq!(*pv.get(i).unwrap(), i);
+    }
+}
+
+#[test]
+fn push_matches_len_cloned() {
+    const N: usize = 5000;
+    let mut pv = PersistentVec::new();
+    for i in 0..N {
+        pv.push(i);
+    }
+    let pv0 = pv.clone();
+    assert_eq!(pv.len(), N);
+    assert_eq!(pv0.len(), N);
+
+    for i in 0..N {
+        pv.push(i);
+    }
+
+    assert_eq!(pv.len(), 2 * N);
+    assert_eq!(pv0.len(), N);
+
+    for i in 0..N {
+        assert_eq!(*pv.get(i).unwrap(), i);
+        assert_eq!(*pv0.get(i).unwrap(), i);
+    }
+
+    for i in 0..N {
+        assert_eq!(*pv.get(i + N).unwrap(), i);
+    }
+}
+
+#[test]
+fn push_matches_mutate_in_place() {
+    const N: usize = BRANCH_FACTOR * 4;
+    let mut pv = PersistentVec::new();
+    for i in 0..N {
+        pv.push(i);
+    }
+    let pv0 = pv.clone();
+    assert_eq!(pv.len(), N);
+    assert_eq!(pv0.len(), N);
+
+    for i in 0..(N/2) {
+        *pv.get_mut(i).unwrap() += 1;
+    }
+
+    assert_eq!(pv.len(), N);
+    assert_eq!(pv0.len(), N);
+
+    for i in 0..(N/2) {
+        assert_eq!(*pv.get(i).unwrap(), i + 1);
+        assert_eq!(*pv0.get(i).unwrap(), i);
+    }
+
+    // the second half ought to be untouched
+    for i in N/2..N {
+        assert_eq!(*pv.get(i).unwrap(), i);
+        assert_eq!(*pv0.get(i).unwrap(), i);
+        assert_eq!(pv.get(i).unwrap() as *const usize, pv0.get(i).unwrap() as *const usize);
     }
 }
